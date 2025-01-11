45607e408ab140f3e2dd72d93fd3ca398ec24f0e
ERXiaomai <1251457056@qq.com>
2025-01-11 11:47:45 +0800

    2025.1.11 2

---
+++ src/api/mainController.ts
@@ -0,0 +1,16 @@
+// @ts-ignore
+/* eslint-disable */
+import request from '@/request'
+
+/** health GET /api/health */
+export async function healthUsingGet(options?: { [key: string]: any }) {
+  return request<API.BaseResponseString_>('/api/health', {
+    method: 'GET',
+    ...(options || {}),
+  })
+}
+
+
+
+
+
