c3e923e4abf8bb1c70387295c1170b3f19e6a6cf
ERXiaomai <1251457056@qq.com>
2025-01-18 11:04:07 +0800

    1.18

--- src/access.ts
+++ src/access.ts
@@ -1,10 +1,30 @@
 import router from '@/router'
 import { useLoginUserStore } from '@/stores/useLoginUserStore.ts'
+import { message } from 'ant-design-vue'
 
+//是否为首次获取登录信息
+let firstFetchLoginUser = true
+
+/**
+ * 全局权限校验,每次切换时都会执行
+ */
 router.beforeEach(async (to, from, next) => {
-  const loginUserStore = useLoginUserStore();
-  let loginUser = loginUserStore.loginUser;
+  const loginUserStore = useLoginUserStore()
+  let loginUser = loginUserStore.loginUser
   // 确保页面刷新时，首次加载时，能等待后端返回用户信息后在校验权限
-  await loginUserStore.fetchLoginUser();
-  loginUser = loginUserStore.loginUser;
+  if (firstFetchLoginUser) {
+    await loginUserStore.fetchLoginUser()
+    loginUser = loginUserStore.loginUser
+    firstFetchLoginUser = false
+  }
+  const toUrl = to.fullPath
+  // 自定义权限校验规则,比如管理员才能访问/admin开头的页面
+  if (toUrl.startsWith('/admin')) {
+    if (!loginUser || loginUser.userRole !== 'admin') {
+      message.error('您没有权限访问该页面')
+      next(`/user/login?redirect=${to.fullPath}`)
+      return
+    }
+  }
+  next() // 继续路由跳转放行
 })
